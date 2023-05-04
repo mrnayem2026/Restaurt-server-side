f70237169f3ed1249db10720c331498b5e11afb0
Mostafizur Rahman nayem <hpbd17062022@gmail.com>
2023-05-04 09:09:43 +0600

    New picture link added in recipes json file

--- index.js
+++ index.js
@@ -6,9 +6,16 @@ const port = process.env.PORT || 3000
 app.use(cors())
 
 const chef = require('./data/chef.json');
+const recipes = require('./data/recipes.json');
 
 app.get('/', (req, res) => {
   res.send(chef)
+})
+
+app.get('/chef/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const selectedRecipe = recipes.find(recipe => recipe.id === id);
+  res.send(selectedRecipe)
 })
 
 app.listen(port, () => {
